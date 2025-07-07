01f00980a631a3f7546668d25373c58cdb5c97f3
TharaniL <tharani4562@gmail.com>
2025-07-07 11:26:36 +0530

    Update: Modified add_shows logic in screen_m.py

--- models/screen_m.py
+++ models/screen_m.py
@@ -1,5 +1,6 @@
 from seat_m import Seat
-
+from show_m import Show
+from copy import deepcopy
 class Screen:
     def __init__(self,scr_id,scr_num,rows,cols):
         self.scr_id=scr_id
@@ -8,10 +9,11 @@ class Screen:
         self.cols=cols
         self.no_of_seats=self.get_no_of_seats()
         self.all_seats=self.create_seats()
-
+        self.shows=[]
     def get_no_of_seats(self):
         return len(self.rows)*self.cols
     def create_seats(self):
+        seats=[]
         for row in self.rows:
             for col in range(self.cols):
                 seat_id=f"{row}{col}"
@@ -25,7 +27,15 @@ class Screen:
                     s_type="Regular"
                     s_rate=100
                 seat=Seat(seat_id,seat_id,s_type,"Available",s_rate)
-                self.all_seats.append(seat)
+                seats.append(seat)
 
 
-        return self.all_seats
+        return seats
+
+    def add_shows(self,sh_id,sh_time,sh_date):
+        show=Show(sh_id,sh_time,sh_date,self.scr_num,deepcopy(self.all_seats))
+        for sh in self.shows:
+            if sh.sh_id==sh_id:
+                return "Dupicate show id are not allowed"
+        self.shows.append(show)
+        return "Show added Successfully"
